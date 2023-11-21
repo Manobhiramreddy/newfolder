ff76ee3b7393655d8f566dd730e1df62a6f48726
Manobhiramreddy <149897547+Manobhiramreddy@users.noreply.github.com>
2023-11-21 20:01:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+let userForm = document.getElementById("u-form");
+
+const retent = () => {
+    let ent = localStorage.getItem("u-details");
+    if (ent) {
+        ent = JSON.parse(ent);
+    } else {
+        ent = [];
+    }
+    return ent;
+};
+
+let UserDet = retent();
+
+const dispent = () => {
+    const ent = retent();
+    let tableEntries = '';
+    for (const entry of ent) {
+        const name1 = `<td>${entry.name}</td>`;
+        const email1 = `<td>${entry.email}</td>`;
+        const password1 = `<td>${entry.password}</td>`;
+        const dob1 = `<td>${entry.dob}</td>`;
+        const acceptTerms1 = `<td>${entry.acceptedTermsAndConditions ? 'true' : 'false'}</td>`;
+
+        const row = `<tr>${name1}${email1}${password1}${dob1}${acceptTerms1}</tr>`;
+        tableEntries += row;
+    }
+    const table = `<table><tr><th>Name</th><th>Email</th><th>Password</th><th>Dob</th><th>Accepted terms?</th></tr>${tableEntries}</table>`;
+    let details = document.getElementById("u-details");
+    details.innerHTML = table;
+};
+
+const saveUserForm = (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptedTermsAndConditions = document.getElementById("acceptTerms").checked;
+
+    const entry = {
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndConditions,
+    };
+    UserDet.push(entry);
+    localStorage.setItem("u-details", JSON.stringify(UserDet));
+    dispent();
+};
+
+userForm.addEventListener("submit", saveUserForm);
+dispent();
